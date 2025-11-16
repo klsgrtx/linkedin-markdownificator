388d8817a2bef98a0534f13d798f18a724640cc6
Martina García Mejía <martina@rifusaki.com>
2025-11-16 09:44:19 -0500

    Swanson8r's HTML parsing and login fixes (#3)
    * swanson8r/fix_login
    * swanson8r/peppermint_twist
    * flesh out main
    * reverted peppermin_twist template reference
    
    ---------
    Co-authored-by: swanson8r <swanson.eric.m@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from utils.retriever import *
 from utils.processer import markdownify
 
+# uncomment and indent before markdownify() to skip scraping and process local files.
+#if not os.path.isfile('./data/languages.html'):
+# TODO add a flag with prompt to skip or overwrite if files already exist.
+
 # Do not forget to modify .env
 load_dotenv()
 
 profile_url = login_to_profile(os.getenv("MAIL"), os.getenv("PASSWORD"))
 
-download_profile(profile_url, ["honors",])
+download_profile(profile_url, ["honors",]) # omit (do not download) details/honors/ ; not included in templates.
 
 WebDriver.get_instance().quit()
 
